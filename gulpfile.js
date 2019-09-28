526130a5326acb9aabd82b923a514d5c68b4f041
Diego Gobbi Slomp <diegogslomp@gmail.com>
2019-09-28 14:59:57 -0300

    13. CSS File Architecture

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,25 @@ const gulp = require('gulp');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssvars = require('postcss-simple-vars');
-const nested = require('postcss-nested')
+const nested = require('postcss-nested');
+const cssImport = require('postcss-import');
 
-gulp.task('default', function () {
-  console.log("Hooray - you created a Gulp task.");
+gulp.task('default', function() {
+  console.log('Hooray - you created a Gulp task.');
 });
 
-gulp.task('html', function () {
-  console.log("Imagine something useful being done to your HTML here.");
+gulp.task('html', function() {
+  console.log('Imagine something useful being done to your HTML here.');
 });
 
-gulp.task('styles', function () {
-  return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([autoprefixer, cssvars, nested]))
+gulp.task('styles', function() {
+  return gulp
+    .src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 });
 
-gulp.task('watch', function (done) {
+gulp.task('watch', function(done) {
   gulp.watch('./app/index.html', gulp.series('html'));
   gulp.watch('./app/assets/styles/**/*.css', gulp.series('styles'));
   done();
