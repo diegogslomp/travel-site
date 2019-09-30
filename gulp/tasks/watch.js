269c1e8b0885f70073436de586eab78e6a5bbd93
Diego Gobbi Slomp <diegogslomp@gmail.com>
2019-09-30 09:16:20 -0300

    17. Organizing Gulpfile

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+
+gulp.task('watch', function () {
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "app"
+        }
+    })
+    gulp.watch('./app/index.html').on('change', browserSync.reload);
+    gulp.watch('./app/assets/styles/**/*.css', gulp.series('styles', 'cssInject'));
+});
+
+
+gulp.task('cssInject', function () {
+    return gulp.src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream());
+});
